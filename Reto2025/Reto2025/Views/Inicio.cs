c16fc8d9f5b9d65e6bd438e1e4d7f46e85e10eeb
Rubyviveda <56440799+Rubyviveda@users.noreply.github.com>
2025-01-14 20:14:24 +0100

    calendario parte 1

--- Reto2025/Reto2025/Views/Inicio.cs
+++ Reto2025/Reto2025/Views/Inicio.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -13,9 +14,14 @@ namespace Reto2025.Views
 {
     public partial class Inicio : Form
     {
+      static DateTime inicioMES = DateTime.Now;
         public Inicio()
         {
             InitializeComponent();
+            
+            inicioMES = new DateTime(inicioMES.Year, inicioMES.Month, 1);
+            dias_de_mierda(null);
+
         }
 
         private void tsmi_crearActividades_Click(object sender, EventArgs e)
@@ -106,6 +112,82 @@ namespace Reto2025.Views
             thread.SetApartmentState(ApartmentState.STA);
             thread.Start();
             this.Close();
+        }
+
+        private void dias_de_mierda(int? opMes)
+        {
+          
+
+
+
+            
+
+            label7.Text = inicioMES.Month.ToString();
+
+
+            switch (opMes)
+            {
+                
+                case 1:
+                    inicioMES = inicioMES.AddMonths(-1);
+
+                    break;
+
+                case 2:
+                    inicioMES = inicioMES.AddMonths(1);
+                    break;
+
+                default:
+                    MessageBox.Show("error");
+                    break;
+            }
+            
+            String mesletras = DateTimeFormatInfo.CurrentInfo.GetMonthName(inicioMES.Month);
+
+            lbl_mes.Text = mesletras + "    " + inicioMES.Year; 
+
+
+
+
+            int dias = DateTime.DaysInMonth(inicioMES.Year, inicioMES.Month);
+
+            int dias_semana =Convert.ToInt32(inicioMES.DayOfWeek.ToString("d"));
+            daycontainer.Controls.Clear();
+
+            label1.Text = dias_semana.ToString();
+
+            for (int i = 2; i <= dias_semana; i++)
+            {
+                ControlCalendarioBlanco controlCalendarioBlanco = new ControlCalendarioBlanco();
+                controlCalendarioBlanco.Text = i.ToString();  
+                daycontainer.Controls.Add(controlCalendarioBlanco);
+            }
+            if (inicioMES.Month == 2)
+            {
+                dias = 28;
+            }
+
+            for (int i = 1; i <= dias; i++)
+            {
+                ControlCalendario controlCalendario = new ControlCalendario();
+                controlCalendario.Text = i.ToString();
+                controlCalendario.diaSemana(i);
+                daycontainer.Controls.Add(controlCalendario);
+            }
+
+
+        }
+
+        private void btn_mesAnterior_Click(object sender, EventArgs e)
+        {
+            daycontainer.Controls.Clear();
+            dias_de_mierda(1);
+        }
+
+        private void btn_mesSiguiente_Click(object sender, EventArgs e)
+        {
+            daycontainer.Controls.Clear();
+            dias_de_mierda(2);
         }
     }
 }
